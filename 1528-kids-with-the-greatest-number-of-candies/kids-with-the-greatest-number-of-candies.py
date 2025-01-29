43bab5fbd9f48f7fe3e72100252a0995bdaceef3
DavidYu75 <yu.dav@northeastern.edu>
2025-01-29 18:20:02 -0500

    Time: 0 ms (100.00%) | Memory: 12.2 MB (82.75%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    # greatest can be equal to or greater than max
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        # brute force:
+        # time: O(n^2)
+        # space: O(n)
+        # result = []
+        
+        # for i in range(len(candies)):
+        #     totalCandies = candies[i] + extraCandies
+        #     result.append(True)
+        #     for j in range(len(candies)):
+        #         if candies[j] > totalCandies:
+        #             result.pop()
+        #             result.append(False)
+        
+        # return result
+        
+        # time: O(n)
+        # space: O(n)
+        maxCandies = 0
+        result = []
+
+        for candy in candies:
+            if candy > maxCandies:
+                maxCandies = candy
+        
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= maxCandies:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result
+        
