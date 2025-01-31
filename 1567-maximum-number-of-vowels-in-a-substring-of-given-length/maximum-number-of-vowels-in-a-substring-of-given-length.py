c5ee78f71aed0c403bade6233f4d65eb2d729d1c
DavidYu75 <yu.dav@northeastern.edu>
2025-01-31 00:07:06 -0500

    Time: 98 ms (41.48%) | Memory: 18.2 MB (16.43%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,26 +1,48 @@
 class Solution:
     # time: O(n)
-    # space: O(n)
+    # space: O(1)
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-        char_window = s[:k]
-        max_vowel_count = 0
+        #vowels = {'a', 'e', 'i', 'o', 'u'}
+        # vowels = "aeiou"
+        # char_window = s[:k]
+        # max_vowel_count = 0
 
-        for char in list(char_window):
-            if char in vowels:
-                max_vowel_count += 1
+        # for char in list(char_window):
+        #     if char in vowels:
+        #         max_vowel_count += 1
 
-        vowel_count = max_vowel_count
+        # vowel_count = max_vowel_count
 
-        for i in range(k, len(s)):
-            if char_window[i - k] in vowels:
-                vowel_count -= 1
-            if s[i:i+1] in vowels:
-                vowel_count += 1
+        # for i in range(k, len(s)):
+        #     if char_window[i - k] in vowels:
+        #         vowel_count -= 1
+        #     if s[i:i+1] in vowels:
+        #         vowel_count += 1
 
-            char_window += s[i:i+1]
+        #     char_window += s[i:i+1]
 
-            if vowel_count > max_vowel_count:
-                max_vowel_count = vowel_count
+        #     if vowel_count > max_vowel_count:
+        #         max_vowel_count = vowel_count
         
-        return max_vowel_count
+        # return max_vowel_count
+
+        # Maximum vowels i.e. ans
+        ans: int = 0
+            
+        # Vowels in current window
+        currCount: int = 0
+            
+        # String of vowels
+        vowels: str = "aeiou"
+            
+        # Using sliding window technique to 
+        # calculate number of vowels in each window and 
+        # update the count
+        for i, v in enumerate(s):
+            if i >= k:
+                if s[i-k] in vowels:
+                    currCount -= 1
+            if s[i] in vowels:
+                currCount += 1
+            ans = max(currCount, ans)
+        return ans
