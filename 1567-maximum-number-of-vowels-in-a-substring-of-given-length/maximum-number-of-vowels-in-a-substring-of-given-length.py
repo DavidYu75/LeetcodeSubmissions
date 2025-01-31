7282c9de63b032cc4dc84b3899b4c0029ebc7ccc
DavidYu75 <yu.dav@northeastern.edu>
2025-01-31 00:09:18 -0500

    Time: 79 ms (63.28%) | Memory: 18.8 MB (8.92%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -2,36 +2,25 @@ class Solution:
     # time: O(n)
     # space: O(1)
     def maxVowels(self, s: str, k: int) -> int:
-        #vowels = {'a', 'e', 'i', 'o', 'u'}
-        # vowels = "aeiou"
-        # char_window = s[:k]
-        # max_vowel_count = 0
+        vowels = "aeiou"
+        char_window = s[:k]
+        max_vowel_count = 0
 
-        # for char in list(char_window):
-        #     if char in vowels:
-        #         max_vowel_count += 1
+        for char in list(char_window):
+            if char in vowels:
+                max_vowel_count += 1
 
-        # vowel_count = max_vowel_count
+        vowel_count = max_vowel_count
 
-        # for i in range(k, len(s)):
-        #     if char_window[i - k] in vowels:
-        #         vowel_count -= 1
-        #     if s[i:i+1] in vowels:
-        #         vowel_count += 1
+        for i in range(k, len(s)):
+            if char_window[i - k] in vowels:
+                vowel_count -= 1
+            if s[i:i+1] in vowels:
+                vowel_count += 1
 
-        #     char_window += s[i:i+1]
+            char_window += s[i:i+1]
 
-        #     if vowel_count > max_vowel_count:
-        #         max_vowel_count = vowel_count
+            if vowel_count > max_vowel_count:
+                max_vowel_count = vowel_count
         
-        # return max_vowel_count
-
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-        ans = cnt = 0
-        for i, c in enumerate(s):
-            if c in vowels:
-                cnt += 1
-            if i >= k and s[i - k] in vowels:
-                cnt -= 1
-            ans  = max(cnt, ans)
-        return ans 
+        return max_vowel_count
