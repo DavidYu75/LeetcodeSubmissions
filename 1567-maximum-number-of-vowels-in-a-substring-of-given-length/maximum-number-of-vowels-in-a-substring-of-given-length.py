b07de8be8b10119e28dac1b826f856d183112d8b
DavidYu75 <yu.dav@northeastern.edu>
2025-01-31 00:01:34 -0500

    Time: 119 ms (21.15%) | Memory: 18.9 MB (8.92%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,33 +1,26 @@
 class Solution:
-    # leetcode
-    # k = 3
     # time: O(n)
     # space: O(n)
     def maxVowels(self, s: str, k: int) -> int:
         vowels = ['a', 'e', 'i', 'o', 'u']
-        char_s = list(s)
-        print(char_s)
-        char_window = []
+        char_window = s[:k]
         max_vowel_count = 0
 
-        for i in range(k):
-            char_window.append(char_s[i])
-
-        for char in char_window:
+        for char in list(char_window):
             if char in vowels:
                 max_vowel_count += 1
-        
+
         vowel_count = max_vowel_count
 
         for i in range(k, len(s)):
-            if char_window.pop(0) in vowels:
+            if char_window[i - k] in vowels:
                 vowel_count -= 1
-            if char_s[i] in vowels:
+            if s[i:i+1] in vowels:
                 vowel_count += 1
 
-            char_window.append(char_s[i])
+            char_window += s[i:i+1]
 
             if vowel_count > max_vowel_count:
                 max_vowel_count = vowel_count
-
+        
         return max_vowel_count
