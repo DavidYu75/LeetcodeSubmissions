2df5edbfc2063893c2da3f0ad77bb9d96b8a9c14
DavidYu75 <yu.dav@northeastern.edu>
2025-02-10 22:27:57 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.98%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -13,14 +13,28 @@ class Solution:
     # c     
     # b     false
     def isSubsequence(self, s: str, t: str) -> bool:
-        slow = 0
+        # slow = 0
 
-        while slow < len(s):
-            if s[slow] in t:
-                t = t.split(s[slow],1)[1]
-            else:
-                return False
+        # while slow < len(s):
+        #     if s[slow] in t:
+        #         t = t.split(s[slow],1)[1]
+        #     else:
+        #         return False
 
-            slow += 1
+        #     slow += 1
 
-        return True
+        # return True
+
+        if not s:
+            return True
+
+        s_pointer = 0
+
+        for t_pointer in range(len(t)):
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+
+                if s_pointer == len(s):
+                    return True
+
+        return False
