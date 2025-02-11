839e09cbbeed1e4a62af36323ce759d6b2a7713c
DavidYu75 <yu.dav@northeastern.edu>
2025-02-10 22:22:29 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.98%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -15,14 +15,6 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         slow = 0
 
-        # s_characters = list(s)
-        # sorted_s_characters = sorted(s_characters)
-        # sorted_s_string = "".join(sorted_s_characters)
-
-        # t_characters = list(t)
-        # sorted_t_characters = sorted(t_characters)
-        # sorted_t_string = "".join(sorted_t_characters)
-
         while slow < len(s):
             if s[slow] in t:
                 t = t.split(s[slow],1)[1]
