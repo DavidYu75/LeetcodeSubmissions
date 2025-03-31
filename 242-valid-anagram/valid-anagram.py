8f4574aa220f087c16296ae922fd1787c0a761fc
David Yu <yu.dav@northeastern.edu>
2025-03-30 23:06:52 -0400

    Time: 15 ms (43.37%) | Memory: 18 MB (50.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_chars = list(s)
-        t_chars = list(t)
+        if len(s) != len(t):
+            return False
 
-        s_chars.sort()
-        t_chars.sort()
+        char_count = {}
 
-        return s_chars == t_chars
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        for char in t:
+            if char not in char_count or char_count[char] == 0:
+                return False
+            
+            char_count[char] -= 1
+
+        return True
+        
+        # s_chars = list(s)
+        # t_chars = list(t)
+
+        # s_chars.sort()
+        # t_chars.sort()
+
+        # return s_chars == t_chars
