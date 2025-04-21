bfc01f01b94f1efd4de3a167352b67dde0fb26c2
David Yu <yu.dav@northeastern.edu>
2025-04-20 21:01:44 -0400

    Time: 3 ms (50.43%) | Memory: 19.1 MB (11.44%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,24 +1,35 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # numbers = {}
+        # nums = 2, 7, 11, 15
+        # target = 9
+        # output = 0, 1
 
-        # for i in range(len(nums)):
-        #     numbers[nums[i]] = i
+        # nums = 3, 2, 4
+        # target = 6
+        # output = 1, 2
 
         # for i in range(len(nums)):
         #     complement = target - nums[i]
-        #     if complement in numbers and numbers[complement] != i:
-        #         return [i, numbers[complement]]
-        
-        # return []
+        #     for j in range(len(nums)):
+        #         if j == i:
+        #             continue
+        #         if nums[j] == complement:
+        #             return [i, j]
 
         hashmap = {}
 
+        # 2: 0
+        # 7: 1
+        # 11: 2
+        # 15: 3
+
+        for i in range(len(nums)):
+            hashmap.update({nums[i]: i})
+        
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in hashmap:
+            
+            if complement in hashmap and hashmap[complement] != i:
                 return [i, hashmap[complement]]
-            else:
-                hashmap[nums[i]] = i
         
         return []
