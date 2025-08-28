709481e43e721201e3e65488821ecc1d34030302
David Yu <yu.dav@northeastern.edu>
2025-08-28 00:28:33 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.03%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        close_open = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
 
         for char in s:
-            if char == '(':
-                stack.append(')')
-            elif char == '[':
-                stack.append(']')
-            elif char == '{':
-                stack.append('}')
-            elif len(stack) == 0 or stack.pop() != char:
-                return False
-            
+            if char in close_open:
+                if stack and stack[-1] == close_open[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        
         return len(stack) == 0
