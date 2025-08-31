4cdd28b62acb92beab8cb5ebf4f412f3256d80ae
David Yu <yu.dav@northeastern.edu>
2025-08-31 02:47:02 -0400

    Time: 151 ms (78.23%) | Memory: 19.3 MB (10.15%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        first = 1
+        last = max(piles)
+        # 1 - 11 bananas per hours  that is our search range
+
+        result = -1
+        while first <= last:
+            mid = (first + last) // 2   # 12 // 2 = 6
+
+            hours_taken = 0
+            for bananas in piles:
+                hours_taken += math.ceil(bananas / mid)
+            
+            if hours_taken > h:
+                first = mid + 1
+            else:
+                result = mid
+                last = mid - 1
+
+        return result
+        
