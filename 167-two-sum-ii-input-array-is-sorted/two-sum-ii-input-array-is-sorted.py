2d99369ea2407b2bf746f2ba2b1267779f117d41
David Yu <yu.dav@northeastern.edu>
2025-09-22 02:22:05 -0400

    Time: 5 ms (35.13%) | Memory: 18.4 MB (90.16%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,23 +1,27 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # for i in range(len(numbers)):
-        #     complement = target - numbers[i]
-        #     for j in range(i, len(numbers)):
-        #         if numbers[j] == complement:
-        #             return [i+1, j+1]
-        
-        # return []
+        # we have two pointers. one in the beginning and one in the end
+        # check the sum and compare it with target
+        # if sum is greater than target, we decrement right pointer
+        # if sum is less than target, we increment left pointer
+        # 2, 7, 11, 15  target = 9
+        # left = 2, right = 15
+        # 17 > 9
+        # left = 2, right = 11
+        # 13 > 9
+        # left = 2, right = 7
+        # 9 == 9
 
-        first = 0
-        last = len(numbers) - 1
+        left, right = 0, len(numbers) - 1
 
-        while first < last:
-            current_sum = numbers[first] + numbers[last]
-            if current_sum == target:
-                return [first+1, last+1]
-            elif current_sum > target:
-                last -= 1
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum > target:
+                right -= 1
             elif current_sum < target:
-                first += 1
+                left += 1
+            else:
+                return [left+1, right+1]
         
         return []
