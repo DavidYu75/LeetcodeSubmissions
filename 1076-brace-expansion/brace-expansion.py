9f7a7a825e6d002459f96b26fa6a7a7000393ce8
David Yu <yu.dav@northeastern.edu>
2025-09-25 23:04:51 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (60.66%) - LeetSync

---
+++ 1076-brace-expansion/brace-expansion.py
@@ -0,0 +1,41 @@
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        # parse the s string into a list of options
+        # after we have the group of options, we dfs to generate all possible combinations
+        # time O(n) total number of possible resulting strings * number of groups (length of each resulting string)
+        # R = product of options count, L = length of each resulting string O(n * R * L)
+        # space: O(R * L)
+
+        groups: List[List[str]] = []
+        i, n = 0, len(s)
+
+        while i < n:
+            if s[i] == '{':
+                j = i + 1
+                while j < n and s[j] != '}':
+                    j += 1
+
+                options = s[i+1:j].split(',')
+                options.sort()
+                groups.append(options)
+                i = j + 1
+            else:
+                groups.append([s[i]])
+                i += 1
+        
+        result = []
+        path = []
+
+        def dfs(index):
+            if index == len(groups):
+                result.append(''.join(path))
+                return 
+            
+            for ch in groups[index]:
+                path.append(ch)
+                dfs(index + 1)
+                path.pop()
+
+        dfs(0)
+
+        return result
