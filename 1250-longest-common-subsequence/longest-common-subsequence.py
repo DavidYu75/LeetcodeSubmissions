5b956302a7549a54fbcd49572bb31e20a3ebcfbf
David Yu <yu.dav@northeastern.edu>
2025-10-04 02:45:11 -0400

    Time: 818 ms (23.02%) | Memory: 34.7 MB (80.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,45 +1,20 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        if len(text1) < len(text2):
-            text1, text2 = text2, text1
-
-        prev = [0] * (len(text2) + 1)
-        curr = [0] * (len(text2) + 1)
-
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    curr[j] = 1 + prev[j + 1]
-                else:
-                    curr[j] = max(curr[j + 1], prev[j])
-            prev, curr = curr, prev
-
-        return prev[0]
+        m, n = len(text1), len(text2)
+        cache = [[-1] * n for _ in range(m)]
         
-        # dp = [[0 for j in range(len(text2) + 1)]
-        #          for i in range(len(text1) + 1)]
+        def dfs(i, j):
+            if i >= len(text1) or j >= len(text2):
+                return 0
 
-        # for i in range(len(text1) - 1, -1, -1):
-        #     for j in range(len(text2) - 1, -1, -1):
-        #         if text1[i] == text2[j]:
-        #             dp[i][j] = 1 + dp[i + 1][j + 1]
-        #         else:
-        #             dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+            if cache[i][j] != -1:
+                return cache[i][j]
+            
+            if text1[i] == text2[j]:
+                cache[i][j] = 1 + dfs(i + 1, j + 1)
+            else:
+                cache[i][j] = max(dfs(i + 1, j), dfs(i, j + 1))
 
-        # return dp[0][0]
-        
-        # memo = {}
+            return cache[i][j]
 
-        # def dfs(i, j, memo):
-        #     if i == len(text1) or j == len(text2):  # checking if our pointers reach the end of text1 or text2
-        #         return 0
-        #     if (i, j) in memo:
-        #         return memo[(i, j)]
-        #     if text1[i] == text2[j]:
-        #         memo[(i, j)] = 1 + dfs(i + 1, j + 1, memo)
-        #     else:
-        #         memo[(i, j)] = max(dfs(i + 1, j, memo), dfs(i, j + 1, memo))
-
-        #     return memo[(i, j)]
-
-        # return dfs(0, 0, memo)
+        return dfs(0, 0)
