83b35e26329e61c79a81a054b7942ae3567b4c2e
David Yu <yu.dav@northeastern.edu>
2025-10-05 00:57:07 -0400

    Time: 4 ms (27.17%) | Memory: 19.1 MB (52.51%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -9,20 +9,40 @@ class Solution:
         for n1, n2 in edges:
             adj_list[n1].append(n2)
             adj_list[n2].append(n1)
-        
-        def dfs(i, prev):
-            if i in visited:
-                return False
 
-            visited.add(i)
-            
-            for j in adj_list[i]:
-                if j == prev:
+        queue = deque([(0, -1)])
+        visited.add(0)
+
+        while queue:
+            node, prev = queue.popleft()
+
+            for neighbor in adj_list[node]:
+                if neighbor == prev:
                     continue
                 
-                if not dfs(j, i):
+                if neighbor in visited:
                     return False
-                
-            return True
+
+                queue.append((neighbor, node))
+                visited.add(neighbor)
         
-        return dfs(0, -1) and len(visited) == n
+        return len(visited) == n
+            
+
+        
+        # def dfs(i, prev):
+        #     if i in visited:
+        #         return False
+
+        #     visited.add(i)
+            
+        #     for j in adj_list[i]:
+        #         if j == prev:
+        #             continue
+                
+        #         if not dfs(j, i):
+        #             return False
+                
+        #     return True
+        
+        # return dfs(0, -1) and len(visited) == n
