4db413fc6b4afea987b93a699bf2af70ea9f8bca
David Yu <yu.dav@northeastern.edu>
2025-10-05 19:47:36 -0400

    Time: 4 ms (27.10%) | Memory: 19 MB (66.07%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,9 +1,9 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if not n:
-            return True
+            return False
         
-        adj_list = {i:[] for i in range(n)}
+        adj_list = defaultdict(list)
         visited = set()
 
         for n1, n2 in edges:
@@ -14,35 +14,17 @@ class Solution:
         visited.add(0)
 
         while queue:
-            node, prev = queue.popleft()
+            node, previous = queue.popleft()
 
             for neighbor in adj_list[node]:
-                if neighbor == prev:
-                    continue
-                
+                if neighbor == previous:
+                    continue 
+
                 if neighbor in visited:
                     return False
-
+                
                 queue.append((neighbor, node))
                 visited.add(neighbor)
         
         return len(visited) == n
-            
-
         
-        # def dfs(i, prev):
-        #     if i in visited:
-        #         return False
-
-        #     visited.add(i)
-            
-        #     for j in adj_list[i]:
-        #         if j == prev:
-        #             continue
-                
-        #         if not dfs(j, i):
-        #             return False
-                
-        #     return True
-        
-        # return dfs(0, -1) and len(visited) == n
