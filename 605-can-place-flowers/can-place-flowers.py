8de7e2a1a29cd231750d5347acc58e6390b4c094
David Yu <yu.dav@northeastern.edu>
2025-10-05 22:55:04 -0400

    Time: 7 ms (72.65%) | Memory: 18 MB (82.71%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,36 +1,14 @@
-class Solution(object):
-    # [1, 0, 0, 0, 1] n = 1
-    # [1, 0, 1, 0, 1] true
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
 
-    # [1, 0, 0, 0, 1] n = 2
-    # [1, 0, 1, 0, 1] false
-
-    # [0, 1, 0, 0, 0, 0, 1, 0] n = 3
-    # [0, 1, 0, 1, 0, 0, 1, 0] false
-    def canPlaceFlowers(self, flowerbed, n):
-        """
-        :type flowerbed: List[int]
-        :type n: int
-        :rtype: bool
-        """
-        if len(flowerbed) == 1 and flowerbed[0] == 0:
-            return True
-        elif len(flowerbed) < 1:
-            return False
-
-        available_spots = 0
-
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            available_spots += 1
-            flowerbed[0] = 1
-
-        for i in range(1, len(flowerbed) - 1):
-            if flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0 and flowerbed[i] == 0:
-                available_spots += 1
-                flowerbed[i] = 1
-
-        if flowerbed[len(flowerbed) - 1] == 0 and flowerbed[len(flowerbed) - 2] == 0:
-            available_spots += 1
-            flowerbed[len(flowerbed) - 1] = 1
-
-        return available_spots >= n
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    count += 1
+        
+        return count >= n
