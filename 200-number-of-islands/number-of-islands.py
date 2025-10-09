7714e453caa75eca32d134559f13cc57309696d1
David Yu <yu.dav@northeastern.edu>
2025-10-09 00:02:23 -0400

    Time: 241 ms (67.98%) | Memory: 20 MB (82.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,40 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
         islands = 0
 
-        def bfs(r, c):
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
+                return
+            
             grid[r][c] = "0"
-            queue = deque([(r, c)])
 
-            while queue:
-                r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                dfs(nr, nc)
 
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
 
-                    if nr >= ROWS or nc >= COLS or nr < 0 or nc < 0 or grid[nr][nc] == "0":
-                        continue
+        # def bfs(r, c):
+        #     queue = deque([(r, c)])
+        #     grid[r][c] = "0"
+
+        #     while queue:
+        #         r, c = queue.popleft()
+
+        #         for dr, dc in directions:
+        #             nr, nc = r + dr, c + dc
+
+        #             if nr >= ROWS or nc >= COLS or nr < 0 or nc < 0 or grid[nr][nc] == "0":
+        #                 continue
                     
-                    grid[nr][nc] = "0"
-                    queue.append((nr, nc))
-                        
+        #             queue.append((nr, nc))
+        #             grid[nr][nc] = "0"
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
+                    dfs(r, c)
                     islands += 1
 
         return islands
