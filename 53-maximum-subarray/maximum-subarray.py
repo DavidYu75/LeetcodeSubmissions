210fdc699019a22cb1172a9f5e3142a6a02c05fa
David Yu <yu.dav@northeastern.edu>
2025-10-11 15:40:54 -0400

    Time: 56 ms (64.85%) | Memory: 32 MB (66.23%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,24 +1,40 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # n, result = len(nums), nums[0]
+        # go through every subarray in this array
+        # keep track of the current sum of the subarray
+        # compare it with a max sum
+        # return the max sum
+        # Time complexity of this solution: O(n^2) 
+        # Space complexity: O(1)
 
-        # for i in range(n):
-        #     curr_result = 0
-        #     for j in range(i, n):
-        #         curr_result += nums[j]
-        #         result = max(result, curr_result)
+        # max_sum = nums[0]
+
+        # for i in range(len(nums)):
+        #     curr_sum = 0
+        #     for j in range(i, len(nums)):
+        #         curr_sum += nums[j]
+        #         max_sum = max(max_sum, curr_sum)
         
-        # return result
+        # return max_sum
 
-        result = nums[0]
-        curr_result = 0
+        # nums = [-2,1,-3,4,-1,2,1,-5,4]
+        # -2 + 1 = -1
+        # 1 + -3 = -2 + 4 = 2
+        # -3 
+        # 4
+
+        # curr_sum, max_sum
+        # curr_sum goes negative, we can start fresh with the next element
+        # max_sum
+
+        max_sum = nums[0]
+        curr_sum = 0
 
         for num in nums:
-            if curr_result < 0:
-                curr_result = 0
-            
-            curr_result += num
-            result = max(result, curr_result)
+            if curr_sum < 0:
+                curr_sum = 0
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
         
-        return result
+        return max_sum
         
