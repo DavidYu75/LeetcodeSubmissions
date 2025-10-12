32506bc746820d0248b6a8a822d0cbfd919e0933
David Yu <yu.dav@northeastern.edu>
2025-10-12 13:57:23 -0400

    Time: 51 ms (75.28%) | Memory: 19.5 MB (86.21%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,17 +3,16 @@ class Solution:
         memo = {}
 
         def dfs(i, j):
-            if i == len(word1):
+            if i >= len(word1):
                 return len(word2) - j
-            if j == len(word2):
+            if j >= len(word2):
                 return len(word1) - i
-            
+
             if (i, j) in memo:
                 return memo[(i, j)]
             
             if word1[i] == word2[j]:
                 memo[(i, j)] = dfs(i+1, j+1)
-            
             else:
                 insert_cost = 1 + dfs(i, j+1)
                 delete_cost = 1 + dfs(i+1, j)
@@ -21,7 +20,7 @@ class Solution:
 
                 result = min(insert_cost, delete_cost, replace_cost)
                 memo[(i, j)] = result
-            
+
             return memo[(i, j)]
-        
-        return dfs(0, 0)
+            
+        return dfs(0, 0)        
