1454f3f396e2bf3bf1317e7cdd5cad0683872714
David Yu <yu.dav@northeastern.edu>
2025-10-11 23:50:38 -0400

    Time: 51 ms (75.28%) | Memory: 19.4 MB (90.10%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        memo = {}
+
+        def dfs(i, j):
+            if i == len(word1):
+                return len(word2) - j
+            if j == len(word2):
+                return len(word1) - i
+            
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if word1[i] == word2[j]:
+                memo[(i, j)] = dfs(i+1, j+1)
+            
+            else:
+                insert_cost = 1 + dfs(i, j+1)
+                delete_cost = 1 + dfs(i+1, j)
+                replace_cost = 1 + dfs(i+1, j+1)
+
+                result = min(insert_cost, delete_cost, replace_cost)
+                memo[(i, j)] = result
+            
+            return memo[(i, j)]
+        
+        return dfs(0, 0)
