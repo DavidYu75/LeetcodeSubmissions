18277160fb38ca0b9480ae98f395ee3f6453928d
David Yu <yu.dav@northeastern.edu>
2025-10-12 18:10:46 -0400

    Time: 28 ms (75.64%) | Memory: 21.7 MB (81.48%) - LeetSync

---
+++ 922-possible-bipartition/possible-bipartition.py
@@ -0,0 +1,31 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+
+        for dislike in dislikes:
+            adj_list[dislike[0]].append(dislike[1])
+            adj_list[dislike[1]].append(dislike[0])
+        
+        def bfs(root):
+            queue = deque([root])
+            color[root] = 0
+            while queue:
+                node = queue.popleft()
+
+                for neighbor in adj_list[node]:
+                    if color[neighbor] == color[node]:
+                        return False
+                    if color[neighbor] == -1:
+                        color[neighbor] = 1 - color[node]
+                        queue.append(neighbor)
+            
+            return True
+        
+        color = [-1] * (n+1)
+
+        for i in range(1, n+1):
+            if color[i] == -1:
+                if not bfs(i):
+                    return False
+        
+        return True
