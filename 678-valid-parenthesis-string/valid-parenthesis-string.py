13eaff012522279b53ef625ec1fb3ddc40168f5b
David Yu <yu.dav@northeastern.edu>
2025-10-13 18:46:02 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.07%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,30 +1,47 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        memo = {}
+        left_min, left_max = 0, 0
 
-        def dfs(i, open_count):
-            if open_count < 0:
-                return False
-
-            if i == len(s):
-                return open_count == 0
-
-            if (i, open_count) in memo:
-                return memo[(i, open_count)]
-
-            if s[i] == '(':
-                result = dfs(i+1, open_count+1)
-            elif s[i] == ')':
-                result = dfs(i+1, open_count-1)
+        for c in s:
+            if c == '(':
+                left_min, left_max = left_min + 1, left_max + 1
+            elif c == ')':
+                left_min, left_max = left_min - 1, left_max - 1
             else:
-                result = (
-                    dfs(i+1, open_count) or
-                    dfs(i+1, open_count+1) or
-                    dfs(i+1, open_count-1)
-                )
+                left_min, left_max = left_min - 1, left_max + 1
+            
+            if left_max < 0:
+                return False
+            if left_min < 0:
+                left_min = 0
+        
+        return left_min == 0
+        
+        # memo = {}
 
-            memo[(i, open_count)] = result
+        # def dfs(i, open_count):
+        #     if open_count < 0:
+        #         return False
+
+        #     if i == len(s):
+        #         return open_count == 0
+
+        #     if (i, open_count) in memo:
+        #         return memo[(i, open_count)]
+
+        #     if s[i] == '(':
+        #         result = dfs(i+1, open_count+1)
+        #     elif s[i] == ')':
+        #         result = dfs(i+1, open_count-1)
+        #     else:
+        #         result = (
+        #             dfs(i+1, open_count) or
+        #             dfs(i+1, open_count+1) or
+        #             dfs(i+1, open_count-1)
+        #         )
+
+        #     memo[(i, open_count)] = result
             
-            return memo[(i, open_count)]
+        #     return memo[(i, open_count)]
             
-        return dfs(0, 0)
+        # return dfs(0, 0)
