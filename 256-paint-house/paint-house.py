c85051922154df4b94ad8957a37b47b5cea7070e
David Yu <yu.dav@northeastern.edu>
2025-10-14 00:54:17 -0400

    Time: 8 ms (22.93%) | Memory: 17.9 MB (63.03%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -1,14 +1,19 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
+        # red: 0, blue: 1, green: 2
+
+        # space: O(4*n)
+        # time: O(4*n)
+
         n = len(costs)
         memo = [[-1] * 4 for _ in range(n)]
-
+        
         def dfs(i, prev_color):
             if i == n:
                 return 0
-            
-            if memo[i][prev_color + 1] != -1:
-                return memo[i][prev_color + 1]
+
+            if memo[i][prev_color] != -1:
+                return memo[i][prev_color]
             
             result = float('inf')
             for c in range(3):
@@ -16,8 +21,8 @@ class Solution:
                     continue
                 result = min(result, costs[i][c] + dfs(i+1, c))
             
-            memo[i][prev_color + 1] = result
+            memo[i][prev_color] = result
 
-            return result
+            return memo[i][prev_color]
         
         return dfs(0, -1)
