d19053a6119f36f2038fc8d82e813ba737fb3f31
David Yu <yu.dav@northeastern.edu>
2025-10-15 19:05:28 -0400

    Time: 87 ms (37.52%) | Memory: 20.3 MB (52.21%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,15 +1,8 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        # sliding window
-        # left and right pointer of our window
-        # whenever we run into a 0 and we have flips remaining, use that flip and increase the right pointer
-        # but whenever we run into a 0 and we don't have any flips remaining, increase left pointer
-        # if we remove 0s from our window when we increase our left pointer, then we also want to gain those flips back
-        # max_number that keeps track of our maximum length
-
+        max_count = 0
         left = 0
         zeros_flipped = 0
-        max_count  = 0
 
         for right in range(len(nums)):
             if nums[right] == 0:
