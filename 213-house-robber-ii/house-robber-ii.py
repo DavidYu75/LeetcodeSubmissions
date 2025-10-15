1f2ffbb5198dd569baad43d486e74e1e6dac7c38
David Yu <yu.dav@northeastern.edu>
2025-10-15 16:04:18 -0400

    Time: 3 ms (11.19%) | Memory: 18.1 MB (17.66%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # time: O(n)
+        # space: O(n)
+
+        if len(nums) == 1:
+            return nums[0]
+
+        memo = {}
+
+        def dfs(i, first_house):
+            if (i == len(nums) - 1 and first_house) or i >= len(nums):
+                return 0
+            
+            if (i,first_house) in memo:
+                return memo[(i,first_house)]
+            
+            result = max(dfs(i+1, first_house), nums[i] + dfs(i+2, first_house or i == 0))
+            memo[(i,first_house)] = result
+
+            return memo[(i,first_house)]
+        
+        return max(dfs(0, True), dfs(1, False))
+        
