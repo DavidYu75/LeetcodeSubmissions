ad8794b130e6731c1d0fa9075fa652b6f4ed7412
David Yu <yu.dav@northeastern.edu>
2025-10-17 00:54:31 -0400

    Time: 46 ms (82.85%) | Memory: 32 MB (82.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -6,9 +6,8 @@ class Solution:
         for num in nums:
             if curr_sum < 0:
                 curr_sum = 0
-                
+            
             curr_sum += num
-
             result = max(result, curr_sum)
-
+        
         return result
