ce5fa23914c427cc089f6e30ed517d7f43c206e2
David Yu <yu.dav@northeastern.edu>
2025-10-17 18:28:03 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.04%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        left_min, left_max = 0, 0
+        left_min = left_max = 0
 
         for c in s:
             if c == '(':
@@ -16,32 +16,31 @@ class Solution:
                 left_min = 0
         
         return left_min == 0
+
+        # time: O(n)
+        # space: O(1)
         
         # memo = {}
 
         # def dfs(i, open_count):
         #     if open_count < 0:
         #         return False
-
         #     if i == len(s):
         #         return open_count == 0
-
+            
         #     if (i, open_count) in memo:
         #         return memo[(i, open_count)]
-
+            
         #     if s[i] == '(':
-        #         result = dfs(i+1, open_count+1)
+        #         memo[(i, open_count)] = dfs(i+1, open_count + 1)
         #     elif s[i] == ')':
-        #         result = dfs(i+1, open_count-1)
+        #         memo[(i, open_count)] = dfs(i+1, open_count - 1)
         #     else:
-        #         result = (
-        #             dfs(i+1, open_count) or
-        #             dfs(i+1, open_count+1) or
-        #             dfs(i+1, open_count-1)
-        #         )
-
-        #     memo[(i, open_count)] = result
+        #         memo[(i, open_count)] = (dfs(i+1, open_count + 1) or dfs(i+1, open_count - 1) or dfs(i+1, open_count))
             
         #     return memo[(i, open_count)]
-            
+        
         # return dfs(0, 0)
+
+        # time: O(n)
+        # space: O(n)
