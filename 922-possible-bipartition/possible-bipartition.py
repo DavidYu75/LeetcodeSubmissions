6f6b1244e411735ff7e52d4c589d2efd1f778cd8
David Yu <yu.dav@northeastern.edu>
2025-10-17 00:48:30 -0400

    Time: 30 ms (69.77%) | Memory: 21.7 MB (80.00%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -6,6 +6,8 @@ class Solution:
             adj_list[dislike[0]].append(dislike[1])
             adj_list[dislike[1]].append(dislike[0])
         
+        color = [-1] * (n+1)
+
         def bfs(i):
             queue = deque([i])
             color[i] = 0
@@ -14,16 +16,15 @@ class Solution:
                 node = queue.popleft()
 
                 for neighbor in adj_list[node]:
-                    if color[neighbor] == color[node]:
+                    if color[node] == color[neighbor]:
                         return False
+                    
                     if color[neighbor] == -1:
                         color[neighbor] = 1 - color[node]
                         queue.append(neighbor)
             
             return True
         
-        color = [-1] * (n+1)
-
         for i in range(1, n+1):
             if color[i] == -1:
                 if not bfs(i):
