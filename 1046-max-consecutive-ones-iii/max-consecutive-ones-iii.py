99d0927ffe8ba7de69823c4e9af2a8c4a7e89d1f
David Yu <yu.dav@northeastern.edu>
2025-10-17 22:18:18 -0400

    Time: 87 ms (37.61%) | Memory: 20.5 MB (18.30%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,31 +1,29 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        max_length = left = zero_count = 0
-
-        for right in range(n):
-            if nums[right] == 0:
-                zero_count += 1
-            
-            while zero_count > k:
-                if nums[left] == 0:
-                    zero_count -= 1
-                
-                left += 1
-
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
-
-        # for i in range(n):
-        #     zeros = 0
-        #     for j in range(i, n):
-        #         if nums[j] == 0:
-        #             zeros += 1
-        #         if zeros <= k:
-        #             max_length = max(max_length, j - i + 1)
-        #         else:
-        #             i = j
-        #             break
+        # window that keeps track of 1s in the array
+        # expand this window whenever we encounter a 1
+        # whenever we encounter a 0 and we have flips available, flip this 0 to a 1
+        # whenever we encounter a 0 and we don't have flips available, lets calculate window size
+        # compare this window size with the current max_number 
+        # shrink our window to our current index, and if we remove any 0s from our window, gain those flips back
         
-        # return max_length
+        max_result = 0
+        left = 0
+        zeros_flipped = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros_flipped += 1
+            
+            while zeros_flipped > k:
+                if nums[left] == 0:
+                    zeros_flipped -= 1
+            
+                left += 1
+            
+            max_result = max(max_result, right - left + 1)
+        
+        return max_result
+
+        # time: O(n)
+        # space: O(1)
