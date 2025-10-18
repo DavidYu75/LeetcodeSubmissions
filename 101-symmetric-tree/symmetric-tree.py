47c63e49d1ac86a706a2cc4c8d84c5f44558ff75
David Yu <yu.dav@northeastern.edu>
2025-10-17 20:43:00 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (34.49%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -6,35 +6,17 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        # def dfs(root1, root2):
-        #     if root1 is None and root2 is None:
-        #         return True
-        #     if root1 is None or root2 is None:
-        #         return False
-        #     if root1.val != root2.val:
-        #         return False
+        def dfs(root1, root2):
+            if not root1 and not root2:
+                return True
             
-        #     return root1.val == root2.val and dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
-
-        # return dfs(root, root)
-
-        queue = deque()
-
-        queue.append((root, root))
-
-        while queue:
-            for i in range(len(queue)):
-                root1, root2 = queue.popleft()
-
-                if root1 is None and root2 is None:
-                    continue
-                if root1 is None or root2 is None:
-                    return False
-                if root1.val != root2.val:
-                    return False
-                
-                queue.append((root1.left, root2.right))
-                queue.append((root1.right, root2.left))
+            if not root1 or not root2:
+                return False
+            
+            if root1.val != root2.val:
+                return False
+            
+            return dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
         
-        return True
+        return dfs(root, root)
         
