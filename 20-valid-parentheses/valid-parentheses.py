4f2218696573448eb9176dc68953cf07af557417
David Yu <yu.dav@northeastern.edu>
2025-10-18 00:10:50 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (15.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        close_open = {']': '[', '}': '{', ')': '('}
+        # ()
+        # ()[]{}
+        # ([])
+        # ([)]
+
+        # ([)]
+
+        # stack will be empty at the end if we have a valid string
+        # hashmap, where we have each closing as a key, each value of that key maps to the opening counterpart
+        # O(1)
+        # O(n)
+
+        close_open = {')': '(', ']': '[', '}': '{'}
         stack = []
+
+        # )))
+        # )
 
         for c in s:
             if c in close_open:
@@ -13,3 +28,6 @@ class Solution:
                 stack.append(c)
         
         return len(stack) == 0
+
+        # time: O(n)
+        # space: O(n)
