513800ed1be21540f793dac0dd2bb4de71c9b280
David Yu <yu.dav@northeastern.edu>
2025-10-18 01:16:56 -0400

    Time: 53 ms (83.88%) | Memory: 21.2 MB (20.00%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,6 +7,28 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # binary search O(logn)
+
+        # example 1:
+        # root, p is less than the root and q is greater than the root
+        # p is in the left subtree
+        # q is in the right subtree
+        # return root
+
+        # example 2:
+        # root = 6, p = 2, q = 4
+        # root = 6, p = 7, q = 10
+        # p is less than root and q is also less than root
+        # p and q would both be in the left subtree
+        # whenever p and q diverge and go into different paths, we return the root
+
+        # recursion and dfs
+        # base case: if not root return None
+
+        # max(p, q) < root, search left
+        # min(p, q) > root, search right
+        # if p and q differ, return root
+
         if not root:
             return None
         
@@ -16,3 +38,6 @@ class Solution:
             return self.lowestCommonAncestor(root.right, p, q)
         else:
             return root
+        
+        # time: O(logn)
+        # space: O(h)
