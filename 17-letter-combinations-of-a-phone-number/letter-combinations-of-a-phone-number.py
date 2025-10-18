0f0126c7e461a9073adc7f7aff13b2ed14be3df4
David Yu <yu.dav@northeastern.edu>
2025-10-17 23:17:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.24%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,12 +1,16 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # hashmap, where a key is a digit, the value is the possible letters of that digit
+        # digits = "23"
+        # bf
+        # b
         # backtrack
-        
-        # O(4^n * n)
-        # O(n)
+        # every time our combination equals to the length of our digits, we backtrack and use the next available letter that is mapped to our digits
+        # hashmap, where each digit maps to its letters O(1)
+        # for every letter in our first digit, we will generate all letter combinations with the subsequent letters from our first digit letter
+        # abc
+        # b
 
-        digit_letters = {
+        phone_letters = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -24,10 +28,13 @@ class Solution:
                 result.append(curr_comb)
                 return
             
-            for letter in digit_letters[digits[i]]:
+            for letter in phone_letters[digits[i]]:
                 backtrack(i+1, curr_comb + letter)
         
-        if digits:
-            backtrack(0, "")
         
+        backtrack(0, "")
+
         return result
+
+        # time: O(4^n)
+        # space: O(n)
