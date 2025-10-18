89bd98fc4fe0171b4f60bfd7c33266892ef29daf
David Yu <yu.dav@northeastern.edu>
2025-10-17 20:37:44 -0400

    Time: 7 ms (65.42%) | Memory: 19.1 MB (62.87%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,14 +1,5 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # if our target exists in the array, we want to find the index of our target
-        # if our target doesn't exist, we want to get the index of the closest number in our array
-        # we have two pointers that each point to each neighbor of our target.
-        # 
-        # to find our target: O(logn)
-        # to find all the k integers closest to our target: O(k)
-        # O(logn + k)
-        # O(1)
-
         left, right = 0, len(arr) - 1
 
         while left < right:
@@ -19,8 +10,6 @@ class Solution:
             else:
                 right = middle
         
-        
-        # arr[left] = number that is closest to the x
         left, right = left - 1, left
 
         while right - left - 1 < k:
@@ -34,4 +23,3 @@ class Solution:
                 right += 1
         
         return arr[left+1:right]
-            
