fdf821eec42c20e378bbdcd93f0bcda75e75091c
David Yu <yu.dav@northeastern.edu>
2025-10-19 18:38:21 -0400

    Time: 11 ms (56.02%) | Memory: 25.8 MB (77.21%) - LeetSync

--- 244-shortest-word-distance-ii/shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/shortest-word-distance-ii.py
@@ -1,22 +1,5 @@
 class WordDistance:
 
-    # brute force approach
-    # go through the list and find the index of the first word we want to look for
-    # go through the list again and find the index of the second word
-    # compare the distance
-
-    # optimized approach
-    # we can have a hashmap, where the key is the word and the value of that key is the index where that word shows up
-    # so once we have the indexes of each key, we can then compare and find the shortest indexes between the two target words
-
-    # "practice": [0]
-    # "coding": [1, 9]
-    # 3 and 0, take the absolute value
-    # "makes": [3, 4]
-    # 3 - 1 = 2
-    # 4 - 3 = 1
-
-
     def __init__(self, wordsDict: List[str]):
         self.hashmap = defaultdict(list)
 
@@ -26,19 +9,18 @@ class WordDistance:
     def shortest(self, word1: str, word2: str) -> int:
         l1, l2 = self.hashmap[word1], self.hashmap[word2]
 
-        i = j = 0
+        i, j = 0, 0
         result = float('inf')
 
         while i < len(l1) and j < len(l2):
             result = min(result, abs(l1[i] - l2[j]))
 
-            if l1[i] < l2[j]:
+            if l1[i] <= l2[j]:
                 i += 1
             else:
                 j += 1
         
         return result
-
 
 # Your WordDistance object will be instantiated and called as such:
 # obj = WordDistance(wordsDict)
