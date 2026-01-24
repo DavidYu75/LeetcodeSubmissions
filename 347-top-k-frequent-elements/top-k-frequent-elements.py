dcc014c022180a395134e2db43d955f2a5b477f5
David Yu <yu.dav@northeastern.edu>
2026-01-24 03:14:43 -0500

    Time: 4 ms (70.52%) | Memory: 23 MB (25.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,32 +1,21 @@
-import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
+        freq_map = {}
 
         for num in nums:
-            hashmap[num] = hashmap.get(num, 0) + 1
+            freq_map[num] = freq_map.get(num, 0) + 1
+        
+        min_heap = []
 
-        # k_most = []
+        for num in freq_map.keys():
+            heapq.heappush(min_heap, (freq_map[num], num))
 
-        # while len(k_most) < k:
-        #     most_frequent = 0
-        #     most_frequent_num = 0
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        result = []
 
-        #     for num in hashmap:
-        #         if hashmap[num] > most_frequent:
-        #             most_frequent = hashmap[num]
-        #             most_frequent_num = num
+        for i in range(k):
+            result.append(heapq.heappop(min_heap)[1])
 
-        #     k_most.append(most_frequent_num)
-        #     del hashmap[most_frequent_num]
-
-        heap = []
-
-        for num, freq in hashmap.items():
-            heapq.heappush(heap, [freq, num])
-
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        return [num for freq, num in heap]
+        return result
